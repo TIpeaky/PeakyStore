9e7797b0aad43be82e4a71474ceb5202b1124345
Luiz Afonso <luizcosta12322@yahoo.com>
2022-10-26 15:08:56 -0300

    Created HttpMessageNotReadableException and InvalidFormatException

--- peakystore/src/main/java/com/tipeaky/peakystore/exceptions/GlobalExceptions.java
+++ peakystore/src/main/java/com/tipeaky/peakystore/exceptions/GlobalExceptions.java
@@ -1,7 +1,9 @@
 package com.tipeaky.peakystore.exceptions;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -9,12 +11,25 @@ import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 
-//@ControllerAdvice
-//public class GlobalExceptions {
-//
-//    @ExceptionHandler(EntityNotFoundException.class)
-//    public ResponseEntity<StandardError> entityNotFoundHandlerMethod(EntityNotFoundException e, HttpServletRequest request) {
-//        StandardError se = new StandardError(LocalDateTime.now(), 404, "Not Found", e.getMessage(), request.getRequestURI());
-//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(se);
-//    }
-//}
+@ControllerAdvice
+public class GlobalExceptions {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFoundHandlerMethod(EntityNotFoundException e, HttpServletRequest request) {
+        StandardError se = new StandardError(LocalDateTime.now(), 404, "Not Found", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(se);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<StandardError> HttpMessageNotReadableHandlerMethod(HttpMessageNotReadableException e, HttpServletRequest request) {
+        StandardError se = new StandardError(LocalDateTime.now(), 400, "Bad Request", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(se);
+    }
+
+    @ExceptionHandler(InvalidFormatException.class)
+    public ResponseEntity<StandardError> InvalidFormatExceptionHandlerMethod(InvalidFormatException e, HttpServletRequest request) {
+        StandardError se = new StandardError(LocalDateTime.now(), 400, "Bad Request", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(se);
+    }
+
+}
