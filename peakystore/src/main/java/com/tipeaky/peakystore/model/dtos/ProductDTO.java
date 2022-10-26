a59959a84093ba66f82a22506bfaee1462e3f4db
Luiz Afonso <luizcosta12322@yahoo.com>
2022-10-26 12:02:55 -0300

    Created ProductDTO

---
+++ peakystore/src/main/java/com/tipeaky/peakystore/model/dtos/ProductDTO.java
@@ -0,0 +1,32 @@
+package com.tipeaky.peakystore.model.dtos;
+
+import com.tipeaky.peakystore.model.enums.CategoryEnum;
+import com.tipeaky.peakystore.model.enums.ColorEnum;
+import com.tipeaky.peakystore.model.enums.SectionEnum;
+import com.tipeaky.peakystore.model.enums.SizeEnum;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class ProductDTO {
+
+    private UUID id;
+    private String sku;
+    private String name;
+    private String description;
+    private BigDecimal purchasePrice;
+    private BigDecimal salePrice;
+    private Integer stockQuantity;
+    private String productBrand;
+    private LocalDateTime lastUpdateDate;
+    private ColorEnum color;
+    private SizeEnum size;
+    private CategoryEnum category;
+    private SectionEnum section;
+
+}
