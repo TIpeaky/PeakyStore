fbd375eb9642da2ff34de660db0389c99b752a78
Luiz Afonso <luizcosta12322@yahoo.com>
2022-10-27 13:54:23 -0300

    Added key and description for CategoryEnum

--- peakystore/src/main/java/com/tipeaky/peakystore/model/enums/CategoryEnum.java
+++ peakystore/src/main/java/com/tipeaky/peakystore/model/enums/CategoryEnum.java
@@ -1,6 +1,42 @@
 package com.tipeaky.peakystore.model.enums;
 
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
 public enum CategoryEnum {
-    SHIRT, TSHIRT, BLOUSE, JACKET, SWEATER, SKIRT, DRESS, PANTS, JEANS, SHORTS,
-    SWINSUITS, BATHROBE, PAJAMAS, NIGHTGOWN, PANTS_HOSE, UNDERWEAR, COAT
+    SHIRT("SHI", "Shirt"),
+    TSHIRT("TSH", "Tshirt"),
+    BLOUSE("BLO", "Blouse"),
+    JACKET("JAC", "Jacket"),
+    SWEATER("SWE", "Sweater"),
+    SKIRT("SKI", "Skirt"),
+    DRESS("DRE", "Dress"),
+    PANTS("PAN", "Pants"),
+    JEANS("JEA", "Jeans"),
+    SHORTS("SHO", "Shorts"),
+    SWINSUITS("SWI", "Swingsuits"),
+    BATHROBE("BAT", "Bathrobe"),
+    PAJAMAS("PAJ", "Pajamas"),
+    NIGHTGOWN("NIG", "Nightgown"),
+    PANTS_HOSE("PAN", "Pants Hose"),
+    UNDERWEAR("UND", "Underwear"),
+    COAT("COA", "Coat");
+
+    private String key;
+    private String description;
+
+    CategoryEnum(String key, String description) {
+        this.key = key;
+        this.description = description;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
 }
