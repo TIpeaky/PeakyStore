daa3b5d7d83e784f8bb42a87e46208ddeb2624a3
Luiz Afonso <luizcosta12322@yahoo.com>
2022-10-27 13:55:38 -0300

    Added key and description for SizeEnum

--- peakystore/src/main/java/com/tipeaky/peakystore/model/enums/SizeEnum.java
+++ peakystore/src/main/java/com/tipeaky/peakystore/model/enums/SizeEnum.java
@@ -1,5 +1,31 @@
 package com.tipeaky.peakystore.model.enums;
 
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
 public enum SizeEnum {
-    XS, S, M, L, XL, XXL;
+    XS("XS", "XS Size"),
+    S("S", "S Size"),
+    M("M", "M Size"),
+    L("L", "L Size"),
+    XL("XL", "XL Size"),
+    XXL("XXL", "XXL Size");
+
+    private String key;
+    private String description;
+
+    SizeEnum(String key, String description) {
+        this.key = key;
+        this.description = description;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
 }
