fa80d0706567d1e0fd1a09412bd0f18ea5415b68
Luiz Afonso <luizcosta12322@yahoo.com>
2022-10-27 13:54:42 -0300

    Added key and description for ColorEnum

--- peakystore/src/main/java/com/tipeaky/peakystore/model/enums/ColorEnum.java
+++ peakystore/src/main/java/com/tipeaky/peakystore/model/enums/ColorEnum.java
@@ -1,5 +1,36 @@
 package com.tipeaky.peakystore.model.enums;
 
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
 public enum ColorEnum {
-    BLUE, GREEN, YELLOW, PURPLE, PINK, RED, ORANGE, BROWN, GREY, WHITE, BLACK
+    BLUE("BL", "Blue"),
+    GREEN("GR", "Green"),
+    YELLOW("YE", "Yellow"),
+    PURPLE("PU", "Purple"),
+    PINK("PI", "Pink"),
+    RED("RE", "Red"),
+    ORANGE("OR", "Orange"),
+    BROWN("BR", "Brown"),
+    GREY("GR", "Grey"),
+    WHITE("WH", "White"),
+    BLACK("BA", "Black");
+
+    private String key;
+    private String description;
+
+    ColorEnum(String key, String description) {
+        this.key = key;
+        this.description = description;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
 }
