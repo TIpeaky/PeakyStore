8a72b58052d2653dbe56cba1bd5815805a065fcf
Luiz Afonso <luizcosta12322@yahoo.com>
2022-10-27 13:55:13 -0300

    Added key and description for SectionEnum

--- peakystore/src/main/java/com/tipeaky/peakystore/model/enums/SectionEnum.java
+++ peakystore/src/main/java/com/tipeaky/peakystore/model/enums/SectionEnum.java
@@ -1,6 +1,32 @@
 package com.tipeaky.peakystore.model.enums;
 
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
 public enum SectionEnum {
-    MALE, FEMALE,  UNISEX, MALE_CHILDREN, FEMALE_CHILDREN, UNISEX_CHILDREN;
+    MALE("MA", "Male"),
+    FEMALE("FE", "Female"),
+    UNISEX("UN", "Unisex"),
+    MALE_CHILDREN("MC", "Male Children"),
+    FEMALE_CHILDREN("FC", "Female Children"),
+    UNISEX_CHILDREN("UC", "Unisex Children");
+
+    private String key;
+    private String description;
+
+    SectionEnum(String key, String description) {
+        this.key = key;
+        this.description = description;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
 }
 
