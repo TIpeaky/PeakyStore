1e05088313da88aba43e70f9d810c0019b1be39b
Luiz Afonso <luizcosta12322@yahoo.com>
2022-10-27 13:56:12 -0300

    Added MethodArgumentNotValidHandlerMethod

--- peakystore/src/main/java/com/tipeaky/peakystore/exceptions/GlobalExceptions.java
+++ peakystore/src/main/java/com/tipeaky/peakystore/exceptions/GlobalExceptions.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -27,7 +28,13 @@ public class GlobalExceptions {
     }
 
     @ExceptionHandler(InvalidFormatException.class)
-    public ResponseEntity<StandardError> InvalidFormatExceptionHandlerMethod(InvalidFormatException e, HttpServletRequest request) {
+    public ResponseEntity<StandardError> InvalidFormatHandlerMethod(InvalidFormatException e, HttpServletRequest request) {
+        StandardError se = new StandardError(LocalDateTime.now(), 400, "Bad Request", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(se);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> MethodArgumentNotValidHandlerMethod(MethodArgumentNotValidException e, HttpServletRequest request) {
         StandardError se = new StandardError(LocalDateTime.now(), 400, "Bad Request", e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(se);
     }
