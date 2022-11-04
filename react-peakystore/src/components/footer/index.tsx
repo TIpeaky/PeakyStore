e9d10341c3c1d8b5a679f5a7520487cc4a7bcb05
igor.fernandes <igor.fernandes@foursys.com.br>
2022-11-04 13:04:21 -0300

    Ajuste no componente footer.

---
+++ react-peakystore/src/components/footer/index.tsx
@@ -0,0 +1,11 @@
+import Styles from './Footer.module.scss';
+
+const Footer = () => {
+    return (
+        <footer className={Styles.footer}>
+            <div></div>
+        </footer>
+    )
+}
+
+export default Footer;
--- react-peakystore/src/pages/PageBase/index.tsx
+++ react-peakystore/src/pages/PageBase/index.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom"
 
 import Banner from '../../components/Banner';
 import NavBar from '../../components/NavBar';
-import Rodape from "../../components/footer/Rodape"
+import Rodape from "../../components/footer/Footer"
 
 const PaginaBase = () => {
     return (<main>
