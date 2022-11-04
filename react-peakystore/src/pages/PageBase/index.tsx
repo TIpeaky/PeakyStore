2f708018cb10632f957ec074a21e9709b5bde48c
igor.fernandes <igor.fernandes@foursys.com.br>
2022-11-04 13:07:16 -0300

    Ajuste nome das pastas.

--- react-peakystore/src/pages/PageBase/index.tsx
+++ react-peakystore/src/pages/PageBase/index.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom"
 
 import Banner from '../../components/Banner';
 import NavBar from '../../components/NavBar';
-import Rodape from "../../components/footer/Footer"
+import Rodape from "../../components/Footer/Footer"
 
 const PaginaBase = () => {
     return (<main>
