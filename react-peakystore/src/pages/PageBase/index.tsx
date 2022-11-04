001631b65437a6484bab096863c433de890d05ff
igor.fernandes <igor.fernandes@foursys.com.br>
2022-11-04 10:10:38 -0300

    Instalação do react-router-dom

---
+++ react-peakystore/src/pages/PageBase/index.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom"
+
+import Banner from '../../components/Banner';
+import NavBar from '../../components/NavBar';
+import Rodape from "../../components/footer/Rodape"
+
+const PaginaBase = () => {
+    return (<main>
+        <NavBar />
+        <Banner />
+        <Outlet />
+        <Rodape />
+    </main>)
+}
+
+export default PaginaBase
