8627b392cd9b39c0151dad697139b67763b90d91
= <=>
2022-11-10 10:12:34 -0300

    icons added

--- react-peakystore/src/components/footer/index.tsx
+++ react-peakystore/src/components/footer/index.tsx
@@ -1,9 +1,19 @@
 import Styles from './Footer.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Footer = () => {
     return (
         <footer className={Styles.footer}>
-            <div></div>
+            <div>
+                <p>Receba as nossas ofertas:</p>
+                <input type="email" placeholder='Email' />
+                <button>Ok</button>
+                <span className='redes'>Redes sociais</span>
+                <img src='./images/linkedin-in.svg' alt='Linkedin' />
+                <FontAwesomeIcon icon={['fa-brands', 'fa-linkedin-in']} /> 
+                <FontAwesomeIcon icon="fa-brands fa-instagram" />
+                <FontAwesomeIcon icon="fa-brands fa-twitter" />
+            </div>
         </footer>
     )
 }
