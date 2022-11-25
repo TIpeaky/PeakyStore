facf3583d9d2e67a8dcb487f3088379eb318714a
Luiz Afonso <luizcosta12322@yahoo.com>
2022-11-25 15:59:43 -0300

    Organized imports

--- react-peakystore/src/Rotas/index.tsx
+++ react-peakystore/src/Rotas/index.tsx
@@ -1,5 +1,4 @@
 import { Route, Routes} from 'react-router-dom'
-// import Footer from '../components/Footer'
 import Home from "../pages/Home"
 import LoginAdmin from '../pages/Login/Admin'
 import LoginUsuario from '../pages/Login/User'
@@ -15,10 +14,10 @@ const Rotas = () => {
           </Route>
           <Route path='/admin'>
             <Route path='login' element={<LoginAdmin />} />
-          <Route path='/' element={<Home />}>
-
+            <Route path='/' element={<Home />} />
           </Route>
-        </Routes>)
+        </Routes>
+    );
 }
 
 export default Rotas
--- react-peakystore/src/pages/PageBase/index.tsx
+++ react-peakystore/src/pages/PageBase/index.tsx
@@ -1,11 +1,6 @@
 import { Outlet } from "react-router-dom"
-
-import Banner from '../../components/Banner';
-import NavBar from '../../components/NavBar';
 import Rodape from "../../components/footer/index"
-
-import Rodape from "../../components/Footer"
-import Header from "../../components/Header"
+import Header from "../../components/header"
 const PaginaBase = () => {
     return (<main>
         <Header/>
