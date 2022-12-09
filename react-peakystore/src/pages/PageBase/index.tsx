4e2e342ffa7f6f98c43d41d4172545e73676894f
igor.fernandes <igor.fernandes@foursys.com.br>
2022-12-09 09:27:47 -0300

    AJuste import de componentes

--- react-peakystore/src/pages/PageBase/index.tsx
+++ react-peakystore/src/pages/PageBase/index.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom"
-import Rodape from "../../components/footer"
-import Header from "../../components/header"
+import Rodape from "../../components/Footer"
+import Header from "../../components/Header"
 const PaginaBase = () => {
     return (<main>
         <Header/>
