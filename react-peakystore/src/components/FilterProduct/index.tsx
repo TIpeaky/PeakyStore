738bac1c3171c8a05287674f54f6603fc6496357
igor.fernandes <igor.fernandes@foursys.com.br>
2022-12-09 09:12:22 -0300

    CIriando Componente de filtrar produtos

---
+++ react-peakystore/src/components/FilterProduct/index.tsx
@@ -0,0 +1,17 @@
+import styles from './FilterProduct.module.scss'
+
+function FilterProduct() {
+
+    return (
+        <section>
+            <div>
+                <h3>Categoria</h3>
+
+            </div>
+            
+
+        </section>
+    )
+}
+
+export default FilterProduct
