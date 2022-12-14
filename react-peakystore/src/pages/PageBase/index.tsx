38f11abb99e8bb5d5dd2da63706a904285b45435
Luiz Afonso <luizcosta12322@yahoo.com>
2022-12-14 09:21:00 -0300

    Updated path

--- react-peakystore/src/pages/PageBase/index.tsx
+++ react-peakystore/src/pages/PageBase/index.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom"
-import Rodape from "../../components/footer"
-import Header from "../../components/header/Header"
+import Rodape from "../../components/Footer"
+import Header from "../../components/Header/Header"
 const PaginaBase = () => {
     return (<main>
         <Header/>
