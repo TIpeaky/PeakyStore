f1de2104cb7bc7e349760ab8fa0a6f85a7386e16
rafael.simeoni <rafael.simeoni@foursys.com.br>
2022-12-14 10:28:23 -0300

    correção no import do rodape e header

--- react-peakystore/src/pages/PageBase/index.tsx
+++ react-peakystore/src/pages/PageBase/index.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom"
-import Rodape from "../../components/Footer"
-import Header from "../../components/Header"
+import Rodape from "../../components/footer"
+import Header from "../../components/header"
 const PaginaBase = () => {
     return (<main>
         <Header/>
