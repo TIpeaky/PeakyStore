3769c5d797deec12766747add707cf38f0d9279d
Luiz Afonso <luizcosta12322@yahoo.com>
2022-12-15 15:59:42 -0300

    Added selector options

--- react-peakystore/src/components/OrdinationSelector/index.tsx
+++ react-peakystore/src/components/OrdinationSelector/index.tsx
@@ -1,11 +1,36 @@
+import { useState } from 'react';
 
+// MUI Material
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 const OrdinationSelector = () => {
+  const [option, setOption] = useState('');
 
-    return(
-        <select name="" id=""></select>
-    );
+  const handleChange = (event: SelectChangeEvent) => {
+    setOption(event.target.value as string);
+  };
 
+  return (
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Ordenação</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={option}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value={1}>Preço Crescente</MenuItem>
+          <MenuItem value={2}>Preço Decrescente</MenuItem>
+          <MenuItem value={3}>A - Z</MenuItem>
+          <MenuItem value={3}>Z - A</MenuItem>
+        </Select>
+      </FormControl>
+  );
 }
 
 export default OrdinationSelector;
