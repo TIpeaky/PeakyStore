b780bb2a2be4d806dcd4601137d006e809ef22fd
Luiz Afonso <luizcosta12322@yahoo.com>
2022-12-22 10:34:25 -0300

    Updated getAllProducts method

--- peakystore/src/main/java/com/tipeaky/peakystore/services/ProductService.java
+++ peakystore/src/main/java/com/tipeaky/peakystore/services/ProductService.java
@@ -1,8 +1,11 @@
 package com.tipeaky.peakystore.services;
 
 import com.tipeaky.peakystore.exceptions.MethodNotAllowedException;
+import com.tipeaky.peakystore.filter.GenericSpecificationsBuilder;
+import com.tipeaky.peakystore.filter.SpecificationFactory;
 import com.tipeaky.peakystore.model.dtos.ProductDTO;
 import com.tipeaky.peakystore.model.entities.Product;
+import com.tipeaky.peakystore.model.enums.*;
 import com.tipeaky.peakystore.model.forms.ProductUpdateForm;
 import com.tipeaky.peakystore.model.forms.ProductRegisterForm;
 import com.tipeaky.peakystore.repositories.ProductRepository;
@@ -18,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityNotFoundException;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -26,6 +30,9 @@ public class ProductService {
 
     @Autowired
     ProductRepository productRepository;
+
+    @Autowired
+    private SpecificationFactory<Product> productSpecificationFactory;
 
     @Autowired
     ModelMapper mapper;
@@ -71,9 +78,34 @@ public class ProductService {
         return mapper.map(product, ProductDTO.class);
     }
 
-    public Page<ProductDTO> getAllProducts(Pageable pageable) {
-        Page<Product> productList = productRepository.findAll(pageable);
+    public Page<ProductDTO> getAllProducts(Pageable pageable, ColorEnum color, BrandEnum productBrand, SizeEnum size, CategoryEnum category, SectionEnum section) {
+
+        GenericSpecificationsBuilder<Product> builder = new GenericSpecificationsBuilder<>();
+
+        if(Objects.nonNull(color)) {
+            builder.with(productSpecificationFactory.isEqual("color", color));
+        }
+
+        if(Objects.nonNull(productBrand)) {
+            builder.with(productSpecificationFactory.isEqual("productBrand", productBrand));
+        }
+
+        if(Objects.nonNull(size)) {
+            builder.with(productSpecificationFactory.isEqual("size", size));
+        }
+
+        if(Objects.nonNull(category)) {
+            builder.with(productSpecificationFactory.isEqual("category", category));
+        }
+
+        if(Objects.nonNull(section)) {
+            builder.with(productSpecificationFactory.isEqual("section", section));
+        }
+
+        Page<Product> productList = productRepository.findAll(builder.build(), pageable);
         if(productList.isEmpty()) throw new EntityNotFoundException("Não há produtos cadastrados");
         return productList.map(product -> mapper.map(product, ProductDTO.class));
     }
+
+
 }
